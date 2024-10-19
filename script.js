869c93d177ac0a6eb26e705de863e99d11cf27b2
Nasif Fuad <nasifuad007@gmail.com>
2024-10-19 20:00:16 +0600

    toggle error

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const submit = document.getElementById("submit");
 const box1 = document.getElementById("box1");
 const box2 = document.getElementById("box2");
-const mortage = document.getElementById("mortage");
-const interest = document.getElementById("interest");
-const years = document.getElementById("years");
+const amount = document.getElementById("amount");
+const term = document.getElementById("term");
+const rate = document.getElementById("rate");
+const error_term = document.getElementById("error_term");
+const error_amount = document.getElementById("error_amount");
+const error_rate = document.getElementById("error_rate");
 submit.addEventListener("click", () => {
   if (check_empty() === true) {
     console.log("true");
@@ -11,14 +14,21 @@ submit.addEventListener("click", () => {
     box1.style.display = "flex";
   } else {
     console.log("false");
-    // error();
+    error();
   }
 });
 
 function check_empty() {
-  if (mortage === "" || interest === "" || years === "") {
+  if (amount.value === "" || term.value === "" || rate.value === "") {
     return false;
   } else {
     return true;
   }
 }
+const error = () => {
+  if (amount.value === "" || term.value === "" || rate.value === "") {
+    error_amount.style.display = "block";
+    error_term.style.display = "block";
+    error_rate.style.display = "block";
+  }
+};
