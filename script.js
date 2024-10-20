d852949b329c82e49519c098f73b8cc70c95dda9
Nasif Fuad <nasifuad007@gmail.com>
2024-10-20 06:19:39 +0600

    Mortage  term and rate error shown when empty

--- script.js
+++ script.js
@@ -24,3 +24,36 @@ submit.addEventListener("click", () => {
     error();
   }
 });
+
+function check_empty() {
+  const amount_value = amount.value;
+  const term_value = term.value;
+  const rate_value = rate.value;
+  if (amount_value === "") {
+    error_amount.style.display = "block";
+    euro.classList.add("erreuro");
+    ammount.classList.add("red");
+  } else {
+    error_amount.style.display = "none";
+    euro.classList.remove("erreuro");
+    ammount.classList.remove("red");
+  }
+  if (rate_value === "") {
+    error_rate.style.display = "block";
+    percentage.classList.add("err");
+    ratee.classList.add("red");
+  } else {
+    error_rate.style.display = "none";
+    percentage.classList.remove("err");
+    ratee.classList.remove("red");
+  }
+  if (term_value === "") {
+    error_term.style.display = "block";
+    year.classList.add("err");
+    termm.classList.add("red");
+  } else {
+    error_term.style.display = "none";
+    year.classList.remove("err");
+    termm.classList.remove("red");
+  }
+}
